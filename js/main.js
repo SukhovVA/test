135ef7536827086a12b79c93a2908ce63a055e25
Suhov V <jaxim21@gmail.com>
2020-05-21 14:17:34 +0300

    improve

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ class Product {
  * Массив с заранее созданными товарами
  */
 var arr = [
-    new Product(1,"Товар-1", "Описание товара 1", 1, 100),
+    new Product(1, "Товар-1", "Описание товара 1", 1, 100),
     new Product(2, "Товар 2", "Описание товара 2", 1, 200),
     new Product(3, "Товар 3", "Описание товара 3", 1, 300),
 ];
@@ -31,7 +31,7 @@ arr.forEach(item => appendHtml(item));
  * @returns {number}
  */
 function random() {
-  return Math.round(3 + Math.random() * (10000 - 3));
+    return Math.round(3 + Math.random() * (10000 - 3));
 }
 
 /**
@@ -58,8 +58,7 @@ function appendHtml(product) {
     let productList = document.querySelector('.product');
     let productItem = document.createElement('div');
     productItem.classList.add('product-item');
-    productItem.id = product.id;
-    productItem.innerHTML = '<div class="product-item__header">' + product.name + '</div><img src="https://via.placeholder.com/300x300"><div class="product-item__info">' + product.desc + '</div><div class="product-item__price"> Цена товара : ' + product.price + '</div><div class="product-item__remove" onclick="removeItem(this)"></div><input type="number" min="1" required value="1" oninput="quantityChange(this)">';
+    productItem.innerHTML = '<div class="product-item__header">' + product.name + '</div><img src="https://via.placeholder.com/300x300"><div class="product-item__info">' + product.desc + '</div><div class="product-item__price">' + product.price + ' &#8381;</div><div class="product-item__remove" onclick="removeItem(this)"></div><input type="number" min="1" required value="1" oninput="quantityChange(this)">';
     productList.appendChild(productItem);
 }
 
@@ -68,7 +67,7 @@ function appendHtml(product) {
  * @param el
  */
 function removeItem(el) {
-    arr.splice(findElementIndex(el),1);
+    arr.splice(findElementIndex(el), 1);
     el.parentNode.remove();
 }
 
@@ -99,29 +98,33 @@ function quantityChange(el) {
  */
 function sendData() {
     let data = {
-        TerminalKey : "TinkoffBankTest",
-        Amount : getAmount(), // тут строка, в Receipt - число
-        OrderId : "12354", // по неведомой причине - только числа
-        Description : "Test Order",
-        Receipt : {
-            Email : "test@test.test",
-            Taxation : "osn",
-            Items : getItems()
+        TerminalKey: "TinkoffBankTest",
+        Amount: getAmount(), // тут строка, в Receipt - число
+        OrderId: "12354", // по неведомой причине - только числа
+        Description: "Test Order",
+        Receipt: {
+            Email: "test@test.test",
+            Taxation: "osn",
+            Items: getItems()
 
         }
     }
 
-    console.table(data);
-
     fetch("https://securepay.tinkoff.ru/v2/Init", {
-        method : "POST",
-        body : JSON.stringify(data),
-        headers : {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
             'Content-Type': 'application/json'
         },
-    }).then(response => {
-        return response.json();
-    }).then(data => window.location.href = data.PaymentURL);
+    }).then(response => response.json())
+        .then(data => {
+                if (data.hasOwnProperty("PaymentURL")) {
+                    window.location.href = data.PaymentURL;
+                } else {
+                    alert('Нет товаров!');
+                }
+            }
+        )
 }
 
 /**
@@ -130,7 +133,7 @@ function sendData() {
  */
 function getAmount() {
     let total = 0;
-    arr.forEach(el => total += el.price*el.quantity*100);
+    arr.forEach(el => total += el.price * el.quantity * 100);
     return total.toString();
 }
 
@@ -143,9 +146,9 @@ function getItems() {
     arr.forEach(el => {
         items.push({
             "Name": el.name,
-            "Price": el.price*100,
+            "Price": el.price * 100,
             "Quantity": el.quantity.toString(),
-            "Amount": el.price*el.quantity*100,
+            "Amount": el.price * el.quantity * 100,
             "Tax": "vat10",
         })
     });
